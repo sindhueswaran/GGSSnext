8ac9aa9d3f92d950c3880ae67b11961a1427cf5c
sindhueswaran <54274176+sindhueswaran@users.noreply.github.com>
2022-05-05 13:52:58 +0530

    Add files via upload

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,15 @@
+import React, { useEffect } from "react";
 import '../styles/styles.css'
 import 'bootstrap/dist/css/bootstrap.min.css' 
-import SimpleReactLightbox from 'simple-react-lightbox'
+import SimpleReactLightbox from 'simple-react-lightbox' 
+import TagManager from "react-gtm-module";
 
 function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    TagManager.initialize({ gtmId: "GTM-M4Q7ZZV" });
+  }, []);
+ 
+
   return (
     <SimpleReactLightbox>
       <Component {...pageProps} />
